fbbeb47346a39e0f2731bcd4af676b88aee83748
Micah Zaayer <micahzaayer@gmail.com>
2019-06-14 10:52:28 -0700

    add simple beep return when user enters '1'.

--- js/scripts.js
+++ js/scripts.js
@@ -2,33 +2,41 @@ $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
 
-    const numberInput = $("#userInput").val();
-    const input = numberInput.split("");
-    // const
-    // //loops through vowels array starting a 0
-    //   for(var firstIndex = 0; firstIndex < vowels.length; firstIndex++) {
-    // //looks at first character from input and compares it to current index in vowels array
-    //   if (userInput.charAt(0) === vowels[firstIndex]) {
-    //     console.log(userInput + "way");
-    //   }
-  )};
-);
+    var userInput = parseInt($("#userInput").val());
+    // var input = numberInput.split("");
+    $("#beepOutput").text(beepBoopArray(userInput));
+    $("#beepOutput").show();
+  });
+});
+
+
+  function beepBoopArray(number) {
+    var translatorsArray = [1, 2, 3];
+    var translationsArray = ["Beep!", "Boop!", "I'm sorry..."];
+
+    for (var i = 0; i <= translatorsArray.length; i++) {
+      if (number===translatorsArray[i]) {
+        return translationsArray[i]
+      }
+    }
+
+    // return("?")
 
 
 
 
+  }
+      // for(var firstIndex = 0; firstIndex < input.length; firstIndex++) {
+        // if (input.charAt(0) === "1") {
+        //   console.log(test);
+      // }
 
 
-//     for(var digit = 0; digit < input.length; input++) {
-//       if(input[i] === "1") {
-//         return input[i] = "-";
-//       }
-//     }
-//     var digitReturn = input.join('')
-//     $("#inputOutput").text(digitReturn);
-//     $("#inputOutput").show();
-//   });
-// });
+    // var digitReturn = input.join('')
+
+
+
+
     // for (var digit = 0; digit < input.length; digit++) {
     //   if(input[digit] = digit) {
     //     input[digit] = "input!";
