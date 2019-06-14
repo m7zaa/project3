0a5d991896006ded0e807a763e530a00d02e7062
Micah Zaayer <micahzaayer@gmail.com>
2019-06-14 09:56:48 -0700

    non-working code...

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,42 @@ $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
 
-    var numberInput = $("#userInput").val();
-    var beep = numberInput;
+    const numberInput = $("#userInput").val();
+    const input = numberInput.split("");
+    // const
+    // //loops through vowels array starting a 0
+    //   for(var firstIndex = 0; firstIndex < vowels.length; firstIndex++) {
+    // //looks at first character from input and compares it to current index in vowels array
+    //   if (userInput.charAt(0) === vowels[firstIndex]) {
+    //     console.log(userInput + "way");
+    //   }
+  )};
+);
 
-    $("#beepOutput").text(beep);
-    $("#beepOutput").show();
-  });
-});
+
+
+
+
+
+//     for(var digit = 0; digit < input.length; input++) {
+//       if(input[i] === "1") {
+//         return input[i] = "-";
+//       }
+//     }
+//     var digitReturn = input.join('')
+//     $("#inputOutput").text(digitReturn);
+//     $("#inputOutput").show();
+//   });
+// });
+    // for (var digit = 0; digit < input.length; digit++) {
+    //   if(input[digit] = digit) {
+    //     input[digit] = "input!";
+    //   }
+    // }
+
+
+    // var numberCounter = function(input) {
+    //   if(input = "1") {
+    //     console.log("input");
+    //   }
+    // }
